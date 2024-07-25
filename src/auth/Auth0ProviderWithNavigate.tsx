c1aec0388f32433ba1a446ddc8e6d1997aae9268
Paz Gueta <pazgueta@gmail.com>
2024-07-25 18:19:42 +0300

    manage auth0 with user loggin and logout

--- src/auth/Auth0ProviderWithNavigate.tsx
+++ src/auth/Auth0ProviderWithNavigate.tsx
@@ -1,13 +1,37 @@
-import { AppState, Auth0Provider } from "@auth0/auth0-react";
+import { AppState, Auth0Provider, User } from "@auth0/auth0-react";
 
 type Props = {
   children: React.ReactNode;
 };
 
 const Auth0ProviderWithNavigate = ({ children }: Props) => {
+//   const navigate = useNavigate();
+
+  const domain = import.meta.env.VITE_AUTH0_DOMAIN;
+  const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
+  const redirectUri = import.meta.env.VITE_AUTH0_CALLBACK_URL;
+//   const audience = import.meta.env.VITE_AUTH0_AUDIENCE;
+
+  if (!domain || !clientId || !redirectUri ) {
+    throw new Error("unable to initialise auth");
+  }
+
+  const onRedirectCallback = (appState?: AppState, user?: User) => {
+    console.log("User", user);
+    
+  };
 
   return (
-  
+    <Auth0Provider
+      domain={domain}
+      clientId={clientId}
+      authorizationParams={{
+        redirect_uri: redirectUri,
+      }}
+      onRedirectCallback={onRedirectCallback}
+    >
+      {children}
+    </Auth0Provider>
   );
 };
 
--- src/components/MainNav.tsx
+++ src/components/MainNav.tsx
@@ -1,14 +1,25 @@
+import { useAuth0 } from "@auth0/auth0-react";
 import { Button } from "./ui/button";
+import UserMenu from "./UserMenu";
 
 const MainNav = () => {
+  const { loginWithRedirect, isAuthenticated } = useAuth0(); // redirects the user to the Auth0 Universal Login page.
 
   return (
-    <Button
-        variant="outline"
-        className="font-bold hover:text-orange-500 hover:bg-white"
-    >
-        Log In
-    </Button>
+    <span className="flex space-x-2 items-center">
+      {isAuthenticated ? (
+        <UserMenu />
+      ) : (
+        <Button
+          variant="outline"
+          className="font-bold hover:text-orange-500 hover:bg-white"
+          onClick={async () => await loginWithRedirect()}
+          //After successful authentication, the user is redirected back to your application
+        >
+          Log In
+        </Button>
+      )}
+    </span>
   );
 };
 
