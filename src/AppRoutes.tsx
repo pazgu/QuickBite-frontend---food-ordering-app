d5ba953e10e8ef4e5fc9929517e5e8fcef5ac36c
Paz Gueta <pazgueta@gmail.com>
2024-07-25 12:45:20 +0300

    defined appRoutes holding all routes

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -1,10 +1,11 @@
 import { Navigate, Route, Routes } from "react-router-dom"
+import Layout from "./layouts/layout";
 
 
 const AppRoutes = () => {
     return (
         <Routes>
-            <Route path="/" element={<span>Home Page</span>}/>
+            <Route path="/" element={<Layout>Home page</Layout>}/>
             <Route path="/user-profile" element={<span>USER PROFILE</span>}/>
             <Route path="/*" element={<Navigate to="/"/>}/>
         </Routes>
