750d735a158f0b263fcdf9b8938b68a771b8943b
Paz Gueta <pazgueta@gmail.com>
2024-07-27 18:14:14 +0300

    create user profile page with react form hook and zod

--- src/layouts/layout.tsx
+++ src/layouts/layout.tsx
@@ -8,11 +8,11 @@ type Props = {
   showHero?: boolean;
 };
 
-const Layout = ({children}: Props) => {
+const Layout = ({children, showHero = false}: Props) => {
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
-      <Hero/>
+      {showHero && <Hero/>}
       <div className="container mx-auto flex-1 py-10">{children}</div>
       <Footer/>
     </div>
