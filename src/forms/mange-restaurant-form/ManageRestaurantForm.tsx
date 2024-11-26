d205c6281e8f44c7c530df5e93659a42731162d6
Paz Gueta <pazgueta@gmail.com>
2024-11-26 16:45:41 +0200

    handle onSubmit to manage restaurant form

--- src/forms/mange-restaurant-form/ManageRestaurantForm.tsx
+++ src/forms/mange-restaurant-form/ManageRestaurantForm.tsx
@@ -62,9 +62,45 @@ const ManageRestaurantForm = ({ onSave, isLoading }: Props) => {
     },
   });
 
+  const onSubmit = (formDataJson: RestaurantFormData) => {
+    const formData = new FormData();
+
+    formData.append("restaurantName", formDataJson.restaurantName);
+    formData.append("city", formDataJson.city);
+    formData.append("country", formDataJson.country);
+
+    formData.append(
+      "deliveryPrice",
+      (formDataJson.deliveryPrice * 100).toString()
+    );
+    formData.append(
+      "estimatedDeliveryTime",
+      formDataJson.estimatedDeliveryTime.toString()
+    );
+    formDataJson.cuisines.forEach((cuisine, index) => {
+      formData.append(`cuisines[${index}]`, cuisine);
+    });
+    formDataJson.menuItems.forEach((menuItem, index) => {
+      formData.append(`menuItems[${index}][name]`, menuItem.name);
+      formData.append(
+        `menuItems[${index}][price]`,
+        (menuItem.price * 100).toString()
+      );
+    });
+
+    if (formDataJson.imageFile) {
+      formData.append(`imageFile`, formDataJson.imageFile);
+    }
+
+    onSave(formData);
+  };
+
   return (
     <Form {...form}>
-      <form className="space-y-8 bg-gray-50 p-10 rounded-lg">
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="space-y-8 bg-gray-50 p-10 rounded-lg"
+      >
         <DetailsSection />
         <Separator />
         <CuisinesSection />
