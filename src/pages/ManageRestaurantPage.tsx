73f757946542403aedca1a236bf5a88d3dba1e53
Paz Gueta <pazgueta@gmail.com>
2024-11-27 19:25:34 +0200

    updateMyRestaurant request

--- src/pages/ManageRestaurantPage.tsx
+++ src/pages/ManageRestaurantPage.tsx
@@ -1,18 +1,26 @@
 import {
   useCreateMyRestaurant,
   useGetMyRestaurant,
+  useUpdateMyRestaurant,
 } from "../api/MyRestaurantApi";
 import ManageRestaurantForm from "../forms/mange-restaurant-form/ManageRestaurantForm";
 
 const ManageRestaurantPage = () => {
   const { restaurant } = useGetMyRestaurant();
-  const { createRestaurant, isLoading } = useCreateMyRestaurant();
+  const { createRestaurant, isLoading: isCreateLoading } =
+    useCreateMyRestaurant();
+  const { updateRestaurant, isLoading: isUpdateLoading } =
+    useUpdateMyRestaurant();
+
+  const isEditing = !!restaurant;
+  //whenever the page loads for the first time we check if the restaurant already exists for the user
+  // !! means give me the truthy value of this variable
 
   return (
     <ManageRestaurantForm
       restaurant={restaurant}
-      onSave={createRestaurant}
-      isLoading={isLoading}
+      onSave={isEditing ? updateRestaurant : createRestaurant}
+      isLoading={isCreateLoading || isUpdateLoading}
     />
   );
 };
