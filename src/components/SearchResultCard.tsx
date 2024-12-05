e8b373d2d945061209df88f3dfb67b4f96d6db72
Paz Gueta <pazgueta@gmail.com>
2024-12-05 14:04:57 +0200

    fixed layout to result card

--- src/components/SearchResultCard.tsx
+++ src/components/SearchResultCard.tsx
@@ -21,27 +21,29 @@ const SearchResultCard = ({ restaurant }: Props) => {
           className="rounded-md w-full h-full object-cover"
         />
       </AspectRatio>
-      <h3 className="text-2xl font-bold tracking-tight mb-2 group-hover:underline">
-        {restaurant.restaurantName}
-      </h3>
-      <div id="card-content" className="grid md:grid-cols-2 gap-2">
-        <div className="flex flex-row flex-wrap">
-          {restaurant.cuisines.map((item, index) => (
-            <span className="flex">
-              <span>{item}</span>
-              {index < restaurant.cuisines.length - 1 && <Dot />}
-              {/* so that last cuisine on the list will not have a dot */}
-            </span>
-          ))}
-        </div>
-        <div className="flex gap-2 flex-col">
-          <div className="flex items-center gap-1 text-green-600">
-            <Clock className="text-green-600" />
-            {restaurant.estimatedDeliveryTime} mins
+      <div>
+        <h3 className="text-2xl font-bold tracking-tight mb-2 group-hover:underline">
+          {restaurant.restaurantName}
+        </h3>
+        <div id="card-content" className="grid md:grid-cols-2 gap-2">
+          <div className="flex flex-row flex-wrap">
+            {restaurant.cuisines.map((item, index) => (
+              <span className="flex">
+                <span>{item}</span>
+                {index < restaurant.cuisines.length - 1 && <Dot />}
+                {/* so that last cuisine on the list will not have a dot */}
+              </span>
+            ))}
           </div>
-          <div className="flex items-center gap-1">
-            <Banknote />
-            Delivery from ${(restaurant.deliveryPrice / 100).toFixed(2)}
+          <div className="flex gap-2 flex-col">
+            <div className="flex items-center gap-1 text-green-600">
+              <Clock className="text-green-600" />
+              {restaurant.estimatedDeliveryTime} mins
+            </div>
+            <div className="flex items-center gap-1">
+              <Banknote />
+              Delivery from ${(restaurant.deliveryPrice / 100).toFixed(2)}
+            </div>
           </div>
         </div>
       </div>
