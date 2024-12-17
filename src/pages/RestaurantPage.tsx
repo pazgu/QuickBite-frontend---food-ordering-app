47c06b1e278851a00189a89559d961cb2ab55786
Paz Gueta <pazgueta@gmail.com>
2024-12-17 18:09:26 +0200

    added romoveFrom cart func

--- src/pages/RestaurantPage.tsx
+++ src/pages/RestaurantPage.tsx
@@ -52,6 +52,16 @@ const RestaurantPage = () => {
     });
   };
 
+  const removeFromCart = (cartItem: CartItem) => {
+    setCartItems((prevCartItems) => {
+      const updatedCartItems = prevCartItems.filter(
+        (item) => cartItem._id !== item._id
+      );
+
+      return updatedCartItems;
+    });
+  };
+
   if (isLoading || !restaurant) {
     return "Loading...";
   }
@@ -78,7 +88,11 @@ const RestaurantPage = () => {
 
         <div>
           <Card>
-            <OrderSummary restaurant={restaurant} cartItems={cartItems} />
+            <OrderSummary
+              restaurant={restaurant}
+              cartItems={cartItems}
+              removeFromCart={removeFromCart}
+            />
           </Card>
         </div>
       </div>
